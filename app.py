acf9101503333cdd1ab7f885b9e2d768ae8293a9
Guilherme Freire Azevedo <2401421@salas.aulas>
2025-04-07 19:04:10 -0300

    adicionando app e config

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from config import Config
+from aluno.controller import alunos_bp
+from professor.controller import professores_bp
+from turma.controller import turmas_bp
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+app.register_blueprint(alunos_bp, url_prefix='/alunos')
+app.register_blueprint(professores_bp, url_prefix='/professores')
+app.register_blueprint(turmas_bp, url_prefix='/turmas')
+
+if __name__ == '__main__':
+    app.run(debug=True)
