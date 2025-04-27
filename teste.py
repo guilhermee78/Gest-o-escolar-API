b7ff43bb97d55765026d0d0f673da481b4c9e75f
JONATHAN-DEV-01 <jonathanads2006@gmail.com>
2025-04-27 18:13:51 -0300

    Alteração url

--- teste.py
+++ teste.py
@@ -1,7 +1,8 @@
 import unittest
 import requests
+from config import Config
 
-BASE_URL = "http://127.0.0.1:5000"
+BASE_URL = f"http://127.0.0.1:{Config.PORT}"
 
 class TestBaseAPI(unittest.TestCase):
     
