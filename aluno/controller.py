d2baec67c6e0c73a4e8f5b0ae13d4c7575a43521
Caio Matheus Caetano Silva <2401362@SALAS.AULAS>
2025-04-28 20:28:19 -0300

    Validando entrada de dados - Aluno

--- aluno/controller.py
+++ aluno/controller.py
@@ -1,28 +1,44 @@
 from flask import Blueprint, jsonify, request
 from aluno.model import AlunoModel
 from Util.helpers import validar_campos, validar_numeros
+import re
 
 alunos_bp = Blueprint('alunos', __name__)
+
+
+def validar_nome(nome):
+   
+    if re.match("^[A-Za-záéíóúãõçÁÉÍÓÚÃÕÇ ]+$", nome):
+        return True
+    return False
 
 @alunos_bp.route('/alunos', methods=['GET'])
 def get_alunos():
     alunos = AlunoModel.find_all()
-    return jsonify([aluno.to_dict() for aluno in alunos])  # Usando to_dict
+    return jsonify([aluno.to_dict() for aluno in alunos])  
 
 @alunos_bp.route('/alunos/<int:id_aluno>', methods=['GET'])
 def get_aluno(id_aluno):
     aluno = AlunoModel.find_by_id(id_aluno)
-    return jsonify(aluno.to_dict())  # Usando to_dict
+    return jsonify(aluno.to_dict()) 
 
 @alunos_bp.route('/alunos', methods=['POST'])
 def post_aluno():
     dados = request.get_json()
+    
+    
     valido, erro = validar_campos(dados, ['nome', 'data_nascimento', 'nota_primeiro_semestre', 'nota_segundo_semestre', 'turma_id'])
     if not valido:
         return jsonify({'error': erro}), 400
+    
+   
     valido, erro = validar_numeros(dados, ['nota_primeiro_semestre', 'nota_segundo_semestre'])
     if not valido:
         return jsonify({'error': erro}), 400
+
+   
+    if not validar_nome(dados['nome']):
+        return jsonify({'error': 'Nome inválido. O nome deve conter apenas letras.'}), 400
 
     aluno = AlunoModel(
         nome=dados['nome'],
@@ -32,17 +48,24 @@ def post_aluno():
         turma_id=dados['turma_id']
     )
     aluno.save_to_db()
-    return jsonify(aluno.to_dict()), 201  # Usando to_dict
+    return jsonify(aluno.to_dict()), 201 
 
 @alunos_bp.route('/alunos/<int:id_aluno>', methods=['PUT'])
 def put_aluno(id_aluno):
     dados = request.get_json()
+    
+
     valido, erro = validar_campos(dados, ['nome', 'data_nascimento', 'nota_primeiro_semestre', 'nota_segundo_semestre', 'turma_id'])
     if not valido:
         return jsonify({'error': erro}), 400
+    
     valido, erro = validar_numeros(dados, ['nota_primeiro_semestre', 'nota_segundo_semestre'])
     if not valido:
         return jsonify({'error': erro}), 400
+
+ 
+    if not validar_nome(dados['nome']):
+        return jsonify({'error': 'Nome inválido. O nome deve conter apenas letras.'}), 400
 
     aluno = AlunoModel.find_by_id(id_aluno)
     if aluno:
@@ -52,7 +75,7 @@ def put_aluno(id_aluno):
         aluno.nota_segundo_semestre = dados['nota_segundo_semestre']
         aluno.turma_id = dados['turma_id']
         aluno.save_to_db()
-        return jsonify(aluno.to_dict())  # Usando to_dict
+        return jsonify(aluno.to_dict()) 
     return jsonify({'error': 'Aluno não encontrado'}), 404
 
 @alunos_bp.route('/alunos/<int:id_aluno>', methods=['DELETE'])
